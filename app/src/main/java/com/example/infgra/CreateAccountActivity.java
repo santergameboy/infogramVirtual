d6886deb489275c7598e670437bbc91f5630b2b3
santergameboy <santergameboy@gmail.com>
2021-09-19 20:58:18 -0400

    intent y cardview

---
+++ app/src/main/java/com/example/infgra/CreateAccountActivity.java
@@ -0,0 +1,25 @@
+package com.example.infgra;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+    public void showToolBar(String titulo,boolean botonsubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonsubir);
+
+    }
+}
